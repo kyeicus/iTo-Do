6684abc1e3086b917023d3b4db388d64f4fbafb6
Emmanuel Kyei Nketia <eknketia001@st.ug.edu.gh>
2022-06-16 14:59:44 +0000

    Initial Commit

---
+++ iTo Do/iTo_DoApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTo_DoApp.swift
+//  iTo Do
+//
+//  Created by Emmanuel K. Nketia on 16/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct iTo_DoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
